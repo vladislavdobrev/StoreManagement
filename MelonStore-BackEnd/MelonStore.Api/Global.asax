8a320f67955468202c38a98e32b5b3a204d938e4
vaster <telerik.vasko@gmail.com>
2013-09-14 14:13:08 -0700

    api + datacontext

---
+++ MelonStore-BackEnd/MelonStore.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MelonStore.Api.WebApiApplication" Language="C#" %>
--- MelonStore-BackEnd/MelonStore.Data/MelonStoreContext.cs
+++ MelonStore-BackEnd/MelonStore.Data/MelonStoreContext.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using MelonStore.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MelonStore.Data
 {
-    public class MelonStoreContext
+    public class MelonStoreContext :DbContext
     {
+        public MelonStoreContext()
+            : base("MelonStoreDb")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<StoreProduct> StoreProducts { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Image> Images { get; set; }
     }
 }
