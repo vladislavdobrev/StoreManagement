73caeea41415bdb822813b086b4c747045658952
vaster <telerik.vasko@gmail.com>
2013-09-14 15:27:06 -0700

    many to many fix

--- MelonStore-BackEnd/MelonStore.Models/StoreProduct.cs
+++ MelonStore-BackEnd/MelonStore.Models/StoreProduct.cs
@@ -8,7 +8,9 @@ namespace MelonStore.Models
 {
     public class StoreProduct
     {
+        public int ProductId { get; set; }
         public virtual Product Product { get; set; }
+        public int StoreId { get; set; }
         public virtual Store Store { get; set; }
         public decimal Price { get; set; }
         public int Count { get; set; }
