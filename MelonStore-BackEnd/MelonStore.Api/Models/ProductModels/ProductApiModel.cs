0404783a64abc21e4e32ae8ae90de1bf704a8fad
vaster <telerik.vasko@gmail.com>
2013-09-14 19:27:26 -0700

    ProductStore controller

---
+++ MelonStore-BackEnd/MelonStore.Api/Models/ProductModels/ProductApiModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MelonStore.Api.Models.ProductStoreModels
+{
+    public class ProductApiModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
